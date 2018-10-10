e245de665ec313926d40cf8e2459be4ab62a7b6a
Lawrence Davis <lawrence@lrdiv.co>
2018-10-10 16:49:38 -0500

    Add has-many-query mixin (#2)
    * Add adapter mixin for ember-data-has-many-query addon
    
    * 0.0.2

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -1,8 +1,9 @@
 import ENV from "ember-get-config";
 import ActiveModelAdapter from "active-model-adapter";
 import DataAdapterMixin from "ember-simple-auth/mixins/data-adapter-mixin";
+import { RESTAdapterMixin } from "ember-data-has-many-query";
 
-export default ActiveModelAdapter.extend(DataAdapterMixin, {
+export default ActiveModelAdapter.extend(DataAdapterMixin, RESTAdapterMixin, {
   host: ENV.apiHost,
   namespace: ENV.apiNamespace,
   authorizer: "authorizer:application"
